da40dc1b7e2f378bafd4ac28684280a54137e163
jalak <jalakpatoliya777@gmail.com>
2020-06-01 16:41:30 +0530

    added lazy loading for optimized performance and error-boundary for error handling

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ErrorImageContainer, ErrorImageOverlay, ErrorImageText } from "./error-boundary.styles";
+
+class ErrorBoundary extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            hasError: false
+        }
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.log(error);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <ErrorImageOverlay >
+                    <ErrorImageContainer imageUrl='https://i.imgur.com/lKJiT77.png' />
+                    <ErrorImageText>Sorry, I ate this page 🐶️.</ErrorImageText>
+                </ErrorImageOverlay >
+            )
+        }
+
+        return this.props.children;
+    }
+
+}
+
+export default ErrorBoundary;
